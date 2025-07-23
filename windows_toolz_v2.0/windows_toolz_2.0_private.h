fa2c31b299bd54f3e17ecc15e54027abd4fe3925
ZhangW3944 <MicroCpro@hotmail.com>
2025-07-23 14:53:36 +0800

    windows_toolz_v2.0版本更新
    1-修复了一些已知问题
    2-更新版本2.0
    3-UI全面优化
    4-功能优化添加
    5-完善MIT和版权保护问题

--- windows_toolz_v2.0/windows_toolz_2.0_private.h
+++ windows_toolz_v2.0/windows_toolz_2.0_private.h
@@ -5,19 +5,19 @@
 #define WINDOWS_TOOLZ_2_0_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"2.5.1.5"
+#define VER_STRING	"2.0.1.3"
 #define VER_MAJOR	2
-#define VER_MINOR	5
+#define VER_MINOR	0
 #define VER_RELEASE	1
-#define VER_BUILD	5
+#define VER_BUILD	3
 #define COMPANY_NAME	""
-#define FILE_VERSION	"2.5.1.5"
+#define FILE_VERSION	"2.0.1.3"
 #define FILE_DESCRIPTION	"Developed using the Dev-C++ IDE"
 #define INTERNAL_NAME	""
-#define LEGAL_COPYRIGHT	"Desperate lnc."
+#define LEGAL_COPYRIGHT	"ZhangW3944 lnc."
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	"windows_toolz_v2.0.exe"
 #define PRODUCT_NAME	"windows_toolz_2.0"
-#define PRODUCT_VERSION	"2.5.1.5"
+#define PRODUCT_VERSION	"2.0.1.3"
 
 #endif /*WINDOWS_TOOLZ_2_0_PRIVATE_H*/
