eef0b451d771aada76cf68666d4feba77fe5b992
ZhangW3944 <MicroCpro@hotmail.com>
2025-07-24 17:00:20 +0800

    windows_toolbox_v2.7版本更新
    1-修复了一些已知问题
    2-更新版本2.7
    3-支持中英文切换显示
    4-功能整合添加优化
    5-完善MIT和版权保护问题
    6-管理员密码可更改
    7-系统资源调取功能增强

---
+++ windows_toolbox_v2.7/windows_toolbox_2.7_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef WINDOWS_TOOLBOX_2_7_PRIVATE_H
+#define WINDOWS_TOOLBOX_2_7_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"2.7.2.4"
+#define VER_MAJOR	2
+#define VER_MINOR	7
+#define VER_RELEASE	2
+#define VER_BUILD	4
+#define COMPANY_NAME	""
+#define FILE_VERSION	"2.7.2.4"
+#define FILE_DESCRIPTION	"Developed using the Dev-C++ IDE"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	"ZhangW3944 lnc."
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	"windows_toolbox_2.7.exe"
+#define PRODUCT_NAME	"windows_toolbox_v2.7"
+#define PRODUCT_VERSION	"2.7.2.4"
+
+#endif /*WINDOWS_TOOLBOX_2_7_PRIVATE_H*/
