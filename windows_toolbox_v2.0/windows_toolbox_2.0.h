941e653dfd0c40919d25bd01c245d0eb7c652508
ZhangW3944 <MicroCpro@hotmail.com>
2025-07-24 16:36:08 +0800

    windows_toolbox_v2.0版本更新
    1-修复了一些已知问题
    2-更新版本2.0
    3-UI全面优化
    4-功能优化添加
    5-完善MIT和版权保护问题

---
+++ windows_toolbox_v2.0/windows_toolbox_2.0.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef WINDOWS_TOOLZ_2_0_PRIVATE_H
+#define WINDOWS_TOOLZ_2_0_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"2.0.1.3"
+#define VER_MAJOR	2
+#define VER_MINOR	0
+#define VER_RELEASE	1
+#define VER_BUILD	3
+#define COMPANY_NAME	""
+#define FILE_VERSION	"2.0.1.3"
+#define FILE_DESCRIPTION	"Developed using the Dev-C++ IDE"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	"ZhangW3944 lnc."
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	"windows_toolz_v2.0.exe"
+#define PRODUCT_NAME	"windows_toolz_2.0"
+#define PRODUCT_VERSION	"2.0.1.3"
+
+#endif /*WINDOWS_TOOLZ_2_0_PRIVATE_H*/
